8e1aff04863ec8d0f9c96569f0878f756363fb1f
AhurovRoman <roman.ash.dm@gmail.com>
2025-10-20 13:50:24 +0300

    change task 1

--- src/task1.js
+++ src/task1.js
@@ -1,13 +1,17 @@
 /**
  * @param {number[]} initialArray - Исходный массив
  * @param {number} multiplier - Множитель
- * 
+ *
  * @returns {number[]} Новый массив
- * 
- * @description Возвращает массив с элементами, полученными 
+ *
+ * @description Возвращает массив с элементами, полученными
  * перемножением всех элементов исходного массива на число
  */
 export function multipliedByN(initialArray, multiplier) {
+  const number = [];
+  for (let i = 0; i < initialArray.length; i++) {
+    number.push(initialArray[i] * multiplier);
+  }
 
-  return initialArray;
+  return number;
 }
