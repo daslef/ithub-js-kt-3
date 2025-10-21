a45ae69c16959772062248609ae7437a72278c39
1nterFekT <lavamishals@gmail.com>
2025-10-21 13:36:38 +0300

    Completed all tasks

--- src/task4.js
+++ src/task4.js
@@ -1,12 +1,18 @@
 /**
  * @param {any[]} data - Массив данных
  * @param {Function} callback - Проверочная функция
- * 
+ *
  * @returns {boolean} - Итог проверки
- * 
+ *
  * @description Проверка на наличие в массиве хотя бы
  * одного элемента, удовлетворяющего проверке
  */
 export function hasCorrectElement(data, callback) {
-    return true
+    for (let i = 0; i < data.length; i++) {
+        if (callback(data[i])) {
+            return true;
+        }
+    }
+
+    return false;
 }
