e994fa2c4d23f32368ac5c4442dfbdfc797c5ca3
Киприан Бельшов <belshovka24@spb.ithub.ru>
2025-10-21 13:19:35 +0300

    solve task1

--- src/task1.js
+++ src/task1.js
@@ -8,6 +8,11 @@
  * перемножением всех элементов исходного массива на число
  */
 export function multipliedByN(initialArray, multiplier) {
+  let multiplied = []
 
-  return initialArray;
+  for (let i = 0; i < initialArray.length; i += 1) {
+    multiplied[i] = initialArray[i] * multiplier
+  }
+
+  return multiplied;
 }
