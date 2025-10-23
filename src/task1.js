a6ca7f68a632baa20ca10629f647592fcc77da0e
Maksim <maks.gavrilov.19@mail.ru>
2025-10-23 12:53:03 +0300

    test

--- src/task1.js
+++ src/task1.js
@@ -8,6 +8,10 @@
  * перемножением всех элементов исходного массива на число
  */
 export function multipliedByN(initialArray, multiplier) {
+  const result = [];
+    for (let i = 0; i < initialArray.length; i++) {
+        result.push(initialArray[i] * multiplier);
+    }
 
-  return initialArray;
+  return result;
 }
