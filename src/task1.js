6fc4604d94dd90a2c90d0da9e31268285c3e67a0
Olkhovskiy <steam_limon@mail.ru>
2025-10-26 01:26:02 +0300

    a

--- src/task1.js
+++ src/task1.js
@@ -1,15 +1,10 @@
-/**
- * @param {number[]} initialArray - Исходный массив
- * @param {number} multiplier - Множитель
- * 
- * @returns {number[]} Новый массив
- * 
- * @description Возвращает массив с элементами, полученными 
- * перемножением всех элементов исходного массива на число
- */
 export function multipliedByN(initialArray, multiplier) {
-  for (let ix = 0; ix < initialArray.length; ix++) {
-    initialArray[ix] *= multiplier
+  let multiplied = []
+
+  for (let i = 0; i < initialArray.length; i += 1) {
+    multiplied[i] = initialArray[i] * multiplier
   }
-  return initialArray;
+
+  return multiplied;
 }
+
--- src/task3.js
+++ src/task3.js
@@ -1,9 +1,12 @@
-/**
- * @param {any[]} initialArray - Исходный массив
- * @returns {undefined} 
- * 
- * @description Разворот массива на месте с выводом в консоль
- */
 export function reverseArray(initialArray) {
-    return
+
+    for (let i = 0; i < Math.floor(initialArray.length / 2); i += 1) {
+        let tmp = initialArray[i]
+        initialArray[i] = initialArray[initialArray.length - 1 - i]
+        initialArray[initialArray.length - 1 - i] = tmp
+    }
+
+    console.log(initialArray)
+
+    return undefined
 }
