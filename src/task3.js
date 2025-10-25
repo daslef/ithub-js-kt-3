29b4313ae457b2b42ff3b2d93baf8080c1050e8b
Igor Schwarz <igor12333214@gmail.com>
2025-10-25 12:40:37 +0300

    done

--- src/task3.js
+++ src/task3.js
@@ -1,9 +1,30 @@
 /**
  * @param {any[]} initialArray - Исходный массив
- * @returns {undefined} 
- * 
+ * @returns {undefined}
+ *
  * @description Разворот массива на месте с выводом в консоль
  */
 export function reverseArray(initialArray) {
-    return
+
+  let ix = initialArray.length - 1;
+  let i = 0;
+  while (ix > 0) {
+    [initialArray[i], initialArray[ix]] = [initialArray[ix], initialArray[i]];
+    ix--;
+    i++;
+  }
+  console.log(initialArray);
+  
+  return;
 }
+
+
+// x []
+// [x[0], x[4]] = [x[4], x[0]]
+//
+//  initialArray.push(initialArray[ix])
+//   initialArray.splice(0,initialArray.length/2)
+//   for (let ix = initialArray.length - 1; ix >= 0; ix--) {
+//     [initialArray[i], initialArray[ix]] = [initialArray[ix], initialArray[i]];
+//     i++;
+//   }
--- src/task4.js
+++ src/task4.js
@@ -1,12 +1,20 @@
 /**
  * @param {any[]} data - Массив данных
  * @param {Function} callback - Проверочная функция
- * 
+ *
  * @returns {boolean} - Итог проверки
- * 
+ *
  * @description Проверка на наличие в массиве хотя бы
  * одного элемента, удовлетворяющего проверке
  */
 export function hasCorrectElement(data, callback) {
-    return true
+  let i = 0;
+  callback = false;
+  while (i < data.length) {
+    if (data[i] > 0) callback = true;
+    if(data[i] === " ") callback = true
+
+    i++;
+  }
+  return callback;
 }
