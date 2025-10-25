9e216d7c5f0e908285e0605aeff1edf9b6b6201b
gnrul <antonyganin@gmail.com>
2025-10-25 20:25:25 +0300

    task3

--- src/task3.js
+++ src/task3.js
@@ -5,5 +5,14 @@
  * @description Разворот массива на месте с выводом в консоль
  */
 export function reverseArray(initialArray) {
-    return
+
+    for (let ix = 0; ix < Math.floor(initialArray.length / 2); ix++) {
+        let ixx = initialArray.length - 1 - ix;
+        let temp = initialArray[ix];
+        initialArray[ix] = initialArray[ixx];
+        initialArray[ixx] = temp;
+    }
+    
+    
+    console.log(initialArray);
 }
