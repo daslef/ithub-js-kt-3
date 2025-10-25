ed01e9fad9dea07d88f7232a1c932832cc651292
gnrul <antonyganin@gmail.com>
2025-10-25 20:26:30 +0300

    12

--- src/task1.js
+++ src/task1.js
@@ -7,10 +7,10 @@
  * @description Возвращает массив с элементами, полученными 
  * перемножением всех элементов исходного массива на число
  */
-export function multipliedByN(initialArray, multiplier) {
-  return initialArray.map(initialArray=> initialArray * multiplier)
-//   for (let ix = 0; ix < initialArray.length; ix++) {
-//     initialArray[ix] *= multiplier={}
-//   }
-//   return initialArray;
+// export function multipliedByN(initialArray, multiplier) {
+//   return initialArray.map(initialArray=> initialArray * multiplier)
+// //   for (let ix = 0; ix < initialArray.length; ix++) {
+// //     initialArray[ix] *= multiplier={}
+// //   }
+// //   return initialArray;
 }
