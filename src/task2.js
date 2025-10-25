9eb9683005b2e2cc3cdec3a5d319e869a54fc1bd
gnrul <antonyganin@gmail.com>
2025-10-25 17:29:04 +0300

    task 2

--- src/task2.js
+++ src/task2.js
@@ -6,6 +6,24 @@
  * корректно переводимых из строки в численный вид.
  */
 export function calculateSum(values) {
+  if (values.length === 0) {
+    throw new Error("Передан пустой массив");
+  }
 
-  return 0
+  let sum = 0; 
+  let validNumbers = false; // наличия допустмых чисел
+
+  for (let ix = 0; ix < values.length; ix++) {
+    const num = Number(values[ix]); // преобразование строки в число
+    if (!isNaN(num)) { // проверка допустимого числа
+      sum += num; // Добавление к сумме
+      validNumbers = true; //установка допустимого значен ия
+    }
+  }
+
+  if (!validNumbers) {
+    throw new Error("Отсутствуют численные данные");
+  }
+
+  return sum; 
 }
